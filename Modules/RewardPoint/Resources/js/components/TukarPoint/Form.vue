48e4a8379ca1e3dace1ec867372dc024b110d03a
dandungjn <dandungjnjnjn@gmail.com>
2020-09-04 16:10:33 +0700

    Revisi Menu Tukar Point

--- Modules/RewardPoint/Resources/js/components/TukarPoint/Form.vue
+++ Modules/RewardPoint/Resources/js/components/TukarPoint/Form.vue
@@ -87,6 +87,7 @@
                 agency: false,
                 sales: false,
                 dialog:false,
+                dialog1:false,
                 listStatus:['Aktif', 'Tidak Aktif'],
                 form_data: {
                     category_reward_id: '',
@@ -181,13 +182,8 @@
                 axios.post(this.uri, data)
                     .then((response) => {
                         if (response.data.success) {
-                            this.formAlert = true
-                            this.formAlertState = 'success'
-                            this.formAlertText = response.data.message
+                            this.formAlert = false
 
-                            setTimeout(() => {
-                                this.goto(this.redirectUri);
-                            }, 6000);
                         } else {
                             this.formAlert = true
                             this.formAlertState = 'error'
