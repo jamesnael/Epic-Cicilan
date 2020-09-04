753678f131c95fe9112f3a5e368f4d80f0487417
tonioktoro <tonioktoro@gmail.com>
2020-09-04 15:16:29 +0700

    Fix generate cicilan due date dll

--- Modules/Installment/Http/Controllers/Booking/BookingHelper.php
+++ Modules/Installment/Http/Controllers/Booking/BookingHelper.php
@@ -58,7 +58,11 @@ class BookingHelper
                 $payment['number_of_delays'] = $booking->payments[$i]->number_of_delays;
                 $payment['fine'] = $booking->payments[$i]->fine;
                 $payments[] = $payment;
+                $mth++;
             } else {
+                if (!isset($booking->payments[$i])) {
+                    $mth++;
+                }
                 if ($i == 1) {
                     $credits = $credits - $booking->first_payment;
                 } else {
@@ -73,16 +77,16 @@ class BookingHelper
                 } else {
                     $payment['payment'] = 'Cicilan ' . $i;
                 }
-                $payment['due_date'] = \Carbon\Carbon::parse(get_next_month(get_next_date($booking->due_date), $mth))->format('Y-m-d');
-                $payment['sp1_date'] = $date->addDays(option('sp1_date', 14))->format('Y-m-d');
-                $payment['sp2_date'] = $date->addDays(option('sp2_date', 21))->format('Y-m-d');
-                $payment['sp3_date'] = $date->addDays(option('sp3_date', 28))->format('Y-m-d');
+                $payment['due_date'] = isset($booking->payments[$i]) ? $booking->payments[$i]->due_date : \Carbon\Carbon::parse(get_next_month(get_next_date($booking->due_date), $mth))->format('Y-m-d');
+                $payment['sp1_date'] = isset($booking->payments[$i]) ? $booking->payments[$i]->sp1_date : $date->addDays(option('sp1_date', 14))->format('Y-m-d');
+                $payment['sp2_date'] = isset($booking->payments[$i]) ? $booking->payments[$i]->sp2_date : $date->addDays(option('sp2_date', 21))->format('Y-m-d');
+                $payment['sp3_date'] = isset($booking->payments[$i]) ? $booking->payments[$i]->sp3_date : $date->addDays(option('sp3_date', 28))->format('Y-m-d');
                 $payment['installment'] = $i == 1 ? $booking->first_payment : ($i == $booking->installment_time ? $booking->installment + $credits : $booking->installment);
                 $payment['credit'] = $i == $booking->installment_time ? 0 : $credits;
                 $payment['payment_status'] = 'Unpaid';
                 $payments[] = $payment;
             }
-            $mth++;
+            
         }
 
         if ($booking->payment_type == 'KPR/KPA') {
